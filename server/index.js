b3b39921b7656f27122a044063fa4666850a7142
EdG570 <cole570@hotmail.com>
2018-10-07 15:24:41 -0400

    Added mogodb connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const debug = require('debug')('App')
-
-const config = require('./config/config');
+const morgan = require('morgan')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const config = require('./config/config')
+const db = require('./db')()
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors({ exposedHeaders: 'x-auth-token'}))
+app.use(helmet())
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const port = config.app.port;
 app.listen(port, () => debug(`Express is listening on port: ${ port }`))
